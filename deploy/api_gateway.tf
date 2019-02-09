a0a8dba011314047e336fe82883cb99614ebb7d8
StuartBeacon <stuart@beacondata.com.au>
2019-02-10 06:17:46 +1100

    updating docs

--- deploy/api_gateway.tf
+++ deploy/api_gateway.tf
@@ -34,7 +34,7 @@ resource "aws_api_gateway_integration" "lambda_get" {
   http_method             = "${aws_api_gateway_method.proxy_get.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.api.invoke_arn}"
+  uri                     = "${aws_lambda_function.lambda.invoke_arn}"
 }
 
 resource "aws_api_gateway_method" "proxy_post" {
@@ -60,5 +60,5 @@ resource "aws_lambda_permission" "lambda_permission" {
   action        = "lambda:InvokeFunction"
   function_name = "${var.service_name}"
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:ap-southeast-2:224041885527:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.api.http_method}${aws_api_gateway_resource.api.path}"
+  source_arn = "${aws_api_gateway_deployment.deployment.execution_arn}/*/*"
 }
