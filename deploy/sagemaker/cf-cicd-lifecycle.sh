fb872a338ea6f88a7e1e3460493b8be15c480d30
StuartBeacon <stuart@beacondata.com.au>
2019-02-13 09:50:10 +1100

    tearing down resources for lambda

---
+++ deploy/sagemaker/cf-cicd-lifecycle.sh
@@ -0,0 +1,11 @@
+# Make Directory Sagemaker
+mkdir -p /home/ec2-user/SageMaker/sagemaker-code/
+
+# Copy data from 
+aws s3 cp s3://pocketbook-transaction-data/notebook/notebook.zip /home/ec2-user/SageMaker/notebook.zip
+unzip /home/ec2-user/SageMaker/notebook.zip -d /home/ec2-user/SageMaker/sagemaker-code
+sudo chown -R ec2-user:ec2-user /home/ec2-user/SageMaker/sagemaker-code
+
+#
+tmux new -d -s tmuxbg
+tmux send-keys -t tumuxbg.0 "jupyter notebook nbconvert --execute /home/ec2-user/SageMaker/sagemaker-code/notebook.ipynb"
