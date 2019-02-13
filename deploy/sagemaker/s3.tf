15e54343b2c45bd0dab59dade34c8238c6daaa13
StuartBeacon <stuart@beacondata.com.au>
2019-02-13 22:13:07 +1100

    adding circleci provisioning

--- deploy/sagemaker/s3.tf
+++ deploy/sagemaker/s3.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "sagemaker_bucket" {
 
 resource "aws_s3_bucket_object" "object" {
   provider = "aws.sys_admin"
-  bucket   = "${aws_s3_bucket.bucket.id}"
+  bucket   = "${aws_s3_bucket.sagemaker_bucket.id}"
   key      = "notebook/notebook.zip"
   source   = "${path.module}/../notebook.zip"
 }
