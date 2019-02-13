eccbce7bef7de1e852ac0a05e2f9b11ab462cbc7
StuartBeacon <stuart@beacondata.com.au>
2019-02-13 22:22:19 +1100

    adding circleci provisioning

--- deploy/sagemaker/cf-cicd-lifecycle.sh
+++ deploy/sagemaker/cf-cicd-lifecycle.sh
@@ -2,7 +2,7 @@
 mkdir -p /home/ec2-user/SageMaker/sagemaker-code/
 
 # Copy data from 
-aws s3 cp s3://pocketbook-transaction-data/notebook/notebook.zip /home/ec2-user/SageMaker/notebook.zip
+aws s3 cp s3://dev-sagemaker-bucket/notebook/notebook.zip /home/ec2-user/SageMaker/notebook.zip
 unzip /home/ec2-user/SageMaker/notebook.zip -d /home/ec2-user/SageMaker/sagemaker-code
 sudo chown -R ec2-user:ec2-user /home/ec2-user/SageMaker/sagemaker-code
 
