4ddb92a1f8be482dd8d4fa6af26fd1ae3755707f
StuartBeacon <stuart@beacondata.com.au>
2019-02-17 10:28:11 +1100

    Adding lifecycle to notebook configuration

--- deploy/sagemaker/sagemaker.tf
+++ deploy/sagemaker/sagemaker.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+  region = "ap-southeast-2"
+  alias = "sys_admin"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+  alias = "iam_admin"
+}
+
 resource "aws_sagemaker_notebook_instance" "notebook" {
   provider      = "aws.sys_admin"
   name          = "notebook-spike-instance"
