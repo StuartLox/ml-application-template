4648dd580b55abcc469c23656fe14e1c9d65e00c
StuartBeacon <stuart@beacondata.com.au>
2019-02-18 09:01:45 +1100

    banashing ds store

--- deploy/main.tf
+++ deploy/main.tf
@@ -4,12 +4,10 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-2"
-  alias = "sys_admin"
 }
 
-provider "aws" {
-  region = "ap-southeast-2"
-  alias = "iam_admin"
+provider "terraform-provider-aws" {
+    region = "ap-southeast-2"
 }
 
 module "sagemaker" {
