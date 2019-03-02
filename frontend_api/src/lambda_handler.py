46e38e415ab1cfcc7217cf8687b62b1ff955f1af
StuartBeacon <stuart@beacondata.com.au>
2019-03-02 23:32:08 +1100

    cloudwatch metrics

--- frontend_api/src/lambda_handler.py
+++ frontend_api/src/lambda_handler.py
@@ -19,8 +19,10 @@ def predict():
         # Convert from CSV to pandas
     if flask.request.content_type == 'text/csv':
         data = request.data.decode('utf-8')
-        s = StringIO(data)
+        body = StringIO(data)
+        get_prediction(body)
 
+@app.route("/train", methods=["POST"])
 def train():
     method = request.environ['awsgi.event']['httpsMethod']
     if method == "POST":
@@ -28,12 +30,12 @@ def train():
 
 
 def get_prediction(body):
-    print(body)
     ENDPOINT_NAME = os.environ['ENDPOINTNAME']
     response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
                                        ContentType='text/csv',
-                                       Body="1000025,5,1,1,1,2,1,3,1,1,2")
+                                       Body=body)
     result = json.loads(response['Body'].read().decode())
+    logging.info(result)
     if result > 0.5:
         return  "Benine"
     elif result < 0.5:
