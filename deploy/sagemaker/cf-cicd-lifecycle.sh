994df5c99b9cae129812b5187ed7b704ebad90c4
StuartBeacon <stuart@beacondata.com.au>
2019-03-03 12:07:24 +1100

    unittests for argus-sftp lambda

--- deploy/sagemaker/cf-cicd-lifecycle.sh
+++ deploy/sagemaker/cf-cicd-lifecycle.sh
@@ -1,11 +1,11 @@
 # Make Directory Sagemaker
 mkdir -p /home/ec2-user/SageMaker/sagemaker-code/
 
-# Copy data from 
+# Copy data from S3.
 aws s3 cp s3://dev-sagemaker-bucket/notebook/notebook.zip /home/ec2-user/SageMaker/notebook.zip
 unzip /home/ec2-user/SageMaker/notebook.zip -d /home/ec2-user/SageMaker/sagemaker-code
 sudo chown -R ec2-user:ec2-user /home/ec2-user/SageMaker/sagemaker-code
 
-#
+# Lifecycle configuration
 tmux new -d -s tmuxbg
-tmux send-keys -t tumuxbg.0 "jupyter notebook nbconvert --execute /home/ec2-user/SageMaker/sagemaker-code/notebook.ipynb"
+tmux send-keys -t tmuxbg.0 "jupyter notebook nbconvert --execute /home/ec2-user/SageMaker/sagemaker-code/notebook.ipynb"
