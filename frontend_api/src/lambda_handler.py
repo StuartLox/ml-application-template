cd29021a6b433472da79a3841108cfb2643f0b10
StuartBeacon <stuart@beacondata.com.au>
2019-03-07 14:47:48 +1100

    new docker container

--- frontend_api/src/lambda_handler.py
+++ frontend_api/src/lambda_handler.py
@@ -1,6 +1,8 @@
 import awsgi
 import flask
 import boto3
+import logging
+import json
 
 from flask import (
     Flask,
@@ -30,7 +32,8 @@ def train():
 
 
 def get_prediction(body):
-    ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+    # ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+    ENDPOINT_NAME = "ann-churn-2019-03-07-02-37-01-589"
     runtime = boto3.client('sagemaker-runtime', region_name='ap-southeast-2')
     response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
                                        ContentType='text/csv',
@@ -38,9 +41,11 @@ def get_prediction(body):
     result = json.loads(response['Body'].read().decode())
     logging.info(result)
     if result > 0.5:
-        return  "Benine"
+        data = {"Type": "Benine", "Probability" : result}
+        return  jsonify(data)
     elif result < 0.5:
-        return "Malignant"
+        data = {"Type": "Malignant", "Probability" : result}
+        return jsonify(data)
 
 
 def handler(event, context):
