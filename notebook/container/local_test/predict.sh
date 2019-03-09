ec2901009002c98828f3de425bd56f3b131a17ff
StuartBeacon <stuart@beacondata.com.au>
2019-03-09 12:25:15 +1100

    Testing ml model

--- notebook/container/local_test/predict.sh
+++ notebook/container/local_test/predict.sh
@@ -4,6 +4,8 @@ payload=$1
 content=${2:-text/csv}
 
 curl --connect-timeout 5 \
+     --max-time 10 \
+     --retry 5 \
      --retry-connrefuse \
      --data-binary @${payload} \
      -H "Content-Type: ${content}" \
