410fabefba7ba95e491296e4f85b4ee68be42722
StuartBeacon <stuart@beacondata.com.au>
2019-03-17 17:26:26 +1100

    Inference issue

--- deploy/frontend_api/domain_name.tf
+++ deploy/frontend_api/domain_name.tf
@@ -21,3 +21,9 @@ resource "aws_route53_record" "frontend_api" {
     zone_id                = "${aws_api_gateway_domain_name.frontend_api.regional_zone_id}"
   }
 }
+
+resource "aws_api_gateway_base_path_mapping" "test" {
+  api_id      = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "${aws_api_gateway_deployment.deployment.stage_name}"
+  domain_name = "${aws_api_gateway_domain_name.frontend_api.domain_name}"
+}
