3c3f511b98a26f90c7a57efb2456120865e05417
StuartBeacon <stuart@beacondata.com.au>
2019-03-17 16:54:55 +1100

    Inference issue

---
+++ deploy/frontend_api/domain_name.tf
@@ -0,0 +1,23 @@
+# Resolve Domain Name with API gateway.
+resource "aws_api_gateway_domain_name" "frontend_api" {
+  domain_name              = "api.stuartloxton.com"
+  regional_certificate_arn = "${var.regional_certificate_arn}"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+# Example DNS record using Route53.
+# Route53 is not specifically required; any DNS host can be used.
+resource "aws_route53_record" "frontend_api" {
+  name    = "${aws_api_gateway_domain_name.frontend_api.domain_name}"
+  type    = "A"
+  zone_id = "${var.zone_id}"
+
+  alias {
+    evaluate_target_health = true
+    name                   = "${aws_api_gateway_domain_name.frontend_api.regional_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.frontend_api.regional_zone_id}"
+  }
+}
--- deploy/frontend_api/variables.tf
+++ deploy/frontend_api/variables.tf
@@ -1,3 +1,9 @@
 variable "environment" {default = "dev"}
+
 variable "service_name" {default = "dev-lambda-test"}
+
 variable "endpoint_name" {}
+
+variable "regional_certificate_arn" {}
+
+variable "zone_id" {}
