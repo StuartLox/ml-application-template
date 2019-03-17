de2d27e621dec93a2b25f6c92c51676636c5af7f
StuartBeacon <stuart@beacondata.com.au>
2019-03-17 17:41:31 +1100

    Inference issue

--- deploy/main.tf
+++ deploy/main.tf
@@ -12,7 +12,7 @@ module "sagemaker" {
 
 module "frontend_api" {
   source                   = "./frontend_api"
-  endpoint_name            = "ann-churn-2019-03-07-02-41-44-641"
+  endpoint_name            = "ann-churn-2019-03-14-08-09-53-367"
   zone_id                  = "Z32ZJNY22VS9QO"
   regional_certificate_arn = "arn:aws:acm:ap-southeast-2:224041885527:certificate/090f50d1-caa8-44d9-a6a6-3cd9b752edd0"
 }
